ff26ff3606002dd71299b45daeb3b332a4943ac5
junaidkhatri56 <developerjunaidkhatri@gmail.com>
2024-08-27 23:25:32 +0500

    done

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ var choose = document.querySelector('#choose');
 function toss(choice){
      var tossResult = Math.ceil(Math.random()*2) ;
      if(tossResult === 1 && choice === 'head'){
-        choose.innerHTML = 'YOU CHOOSE HEAD';
+        choose.innerHTML = 'YOU CHOOSE HEADS';
         result.innerHTML = 'CONGRATS! YOU WON THE TOSS';
         image.src = './images/head.png';
      }else if(tossResult === 2 && choice === 'tail'){
-        choose.innerHTML = 'YOU CHOOSE TAIL';
+        choose.innerHTML = 'YOU CHOOSE TAILS';
         result.innerHTML = 'CONGRATS! YOU WON THE TOSS';
         image.src = './images/tail.png';
      }else if(tossResult === 1 && choice === 'tail'){
-        choose.innerHTML = 'YOU CHOOSE TAIL';
+        choose.innerHTML = 'YOU CHOOSE TAILS';
         result.innerHTML = 'OPPS! YOU LOSS THE TOSS';
         image.src = './images/head.png';
      }else if(tossResult === 2 && choice === 'head'){
-        choose.innerHTML = 'YOU CHOOSE HEAD';
+        choose.innerHTML = 'YOU CHOOSE HEADS';
         result.innerHTML = 'OPPS! YOU LOSS THE TOSS';
         image.src = './images/tail.png';
     }
